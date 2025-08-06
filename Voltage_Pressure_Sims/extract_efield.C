17245bcbbc194c4292790c5383dd81d49457b5a5
macostav <mikelacvz@gmail.com>
2025-08-06 14:38:38 -0700

    Investigated E-field.

--- Voltage_Pressure_Sims/extract_efield.C
+++ Voltage_Pressure_Sims/extract_efield.C
@@ -15,7 +15,7 @@ int main() {
   pumaModel.Initialise(
       "/home/macosta/PUMA/miguel_work/Voltage_Pressure_Sims/Comsol_Files/mesh.mphtxt",
       "/home/macosta/ella_work/PUMA_Tests/Simulations/dielectric_py.txt",
-      "/home/macosta/PUMA/miguel_work/Voltage_Pressure_Sims/Comsol_Files/potential_800_new.txt", "mm");
+      "/home/macosta/PUMA/miguel_work/Voltage_Pressure_Sims/Comsol_Files/potential_1600_new.txt", "mm");
 
   // Setup gas
   MediumMagboltz gas;
@@ -44,12 +44,16 @@ int main() {
   const double zMax = 6.0; // cm
   const int nPoints = 500;
 
-  std::ofstream efieldFile("Efield_vs_z.txt");
+  std::ofstream efieldFile("Efield_vs_z_outside.txt");
   efieldFile << "# z [cm]\tEz [V/cm]\n";
+  
+  // Offset from center of PUMA
+  double x = 0.6; // [cm]
+  double y = 0.0; // [cm]
 
   for (int i = 0; i <= nPoints; ++i) {
     double z = zMin + i * (zMax - zMin) / nPoints;
-    std::array<double, 3> efield = pumaModel.ElectricField(0.0, 0.0, z);
+    std::array<double, 3> efield = pumaModel.ElectricField(x, y, z);
     double ez = efield[2];
     efieldFile << z << "\t" << ez << "\n";
   }
@@ -57,12 +61,12 @@ int main() {
   efieldFile.close();
   std::cout << "Electric field profile saved to Efield_vs_z.txt\n";
 
-  std::ofstream potentialFile("Potential_vs_z.txt");
+  std::ofstream potentialFile("Potential_vs_z_outside.txt");
   potentialFile << "# z [cm]\tV [V]\n";
 
 for (int i = 0; i <= nPoints; ++i) {
   double z = zMin + i * (zMax - zMin) / nPoints;
-  double potential = pumaModel.ElectricPotential(0.0, 0.0, z);
+  double potential = pumaModel.ElectricPotential(x, y, z);
   potentialFile << z << "\t" << potential << "\n";
 }
 
--- Voltage_Pressure_Sims/plot_code/efield_plot.py
+++ Voltage_Pressure_Sims/plot_code/efield_plot.py
@@ -2,15 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """
-Plots the electric field vs z for the center of PUMA. This is done with HV 200V.
+Plots the electric field vs z for the center of PUMA.
 """
 if __name__ == "__main__":
     # Load the data
-    z_after, efield_after = np.loadtxt("../Efield_vs_z.txt", unpack=True)
+    z_center, efield_center = np.loadtxt("../Potential_vs_z_center.txt", unpack=True)
+    z_in, efield_in = np.loadtxt("../Potential_vs_z_small_offset.txt", unpack = True)
+    z_out, efield_out = np.loadtxt("../Potential_vs_z_outside.txt", unpack = True)
 
     # Plot
     plt.figure(figsize=(8, 6))
-    plt.plot(z_after, efield_after, marker='o', linestyle='-', color='red', markersize=4)
+    plt.plot(z_center, efield_center, marker='o', linestyle='-', color='red', markersize=4, label = 'Center of PUMA')
+    plt.plot(z_in, efield_in, marker = 'o', linestyle = "-", color = 'blue', markersize=4, label = 'Small Offset from Center (x = 3mm)')
+    plt.plot(z_out, efield_out, marker ='o', linestyle = "-", color = "green", markersize=4, label = 'Outside of Cathode (x = 6mm)')
 
     # Region definitions
     region_edges = [0.307, 0.47, 1.361,2.096, 2.831, 3.566, 4.357, 4.520]  # Define your region boundaries here
@@ -22,13 +26,13 @@ if __name__ == "__main__":
 
     for i in range(len(region_edges) - 1):
         patch = plt.axvspan(region_edges[i], region_edges[i+1],
-                            color=region_colors[i], alpha=0.5, label=region_names[i])
+                            color=region_colors[i], alpha=0.5) # label=region_names[i]
         region_patches.append(patch)
 
     # Labels and title with increased font size
-    plt.title("Electric Potential vs Z (Center of PUMA, 1600V HV)", fontsize=18)
+    plt.title("Electric Potential vs Z (1600V HV)", fontsize=18)
     plt.xlabel("Z [cm]", fontsize=16)
-    plt.ylabel("Electric Field [V/cm]", fontsize=16)
+    plt.ylabel("Electric Potential [V]", fontsize=16)
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
     plt.grid(True)
@@ -36,7 +40,9 @@ if __name__ == "__main__":
     # Combine lines and regions in legend
     line_handles, line_labels = plt.gca().get_legend_handles_labels()
     #plt.legend(handles=line_handles + region_patches, fontsize=11, loc='upper right', frameon=True)
+    plt.legend()
 
     plt.tight_layout()
-    plt.ylim(-100, 400)  # Sets x-axis from 0 to 5
+    #plt.xlim(0.2, 4.7)
+    #plt.ylim(-500,1000)
     plt.savefig("potential_vs_z.png", dpi=300)
